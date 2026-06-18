282c5d2183c673cc807c1f67e7f91d0250bab529
Fernandeslv <fernandes.rosa02@escola.pr.gov.br>
2026-06-18 10:49:46 +0000

    coreeçoes 01

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
-
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
@@ -12,11 +11,10 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
   };
-
 }
+
 const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
-const tempoObjetivo1 = new Date("2026-06-02T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
